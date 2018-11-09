c456ce4118a71fef6159d484156d0b79239b9c82
Matz Radloff <matzradloff@gmail.com>
2018-11-09 11:43:26 +0100

    Updated time and jobscript#2.

--- s5/hochleistungsrechnen/04-PDE/job_script.py
+++ s5/hochleistungsrechnen/04-PDE/job_script.py
@@ -2,9 +2,9 @@ import os
 
 for i in range(12):
     # print('sbatch --partition=west -o out/partdiff-{}c-%A.out -N1 -n1 timescript {} 512'.format(i+1, i+1))
-    os.system('sbatch --partition=west -o partdiff-{}c-%A.out -N1 -n1 timescript {} 512 512'.format(i+1, i+1))
+    os.system('sbatch --partition=west -o out/partdiff-{}c-%A.out -N1 -n1 timescript {} 512 512'.format(i+1, i+1))
 
 for i in range(11):
     interlines = 2**i
     # print('sbatch --partition=west -o out/partdiff-{}i-%A.out -N1 -n1 timescript 12 {}'.format(interlines, interlines))
-    os.system('sbatch --partition=west -o partdiff-{}i-%A.out -N1 -n1 timescript 12 {} 512'.format(interlines, interlines))
+    os.system('sbatch --partition=west -o out/partdiff-{}i-%A.out -N1 -n1 timescript 12 {} 512'.format(interlines, interlines))
