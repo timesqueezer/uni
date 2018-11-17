c911b1325ec04d024b5279030577b2d8a8c9ee82
Matz Radloff <matzradloff@gmail.com>
2018-11-17 23:12:45 +0100

    Fixed job_script.

--- s5/hochleistungsrechnen/05-PDE/job_script.py
+++ s5/hochleistungsrechnen/05-PDE/job_script.py
@@ -2,4 +2,4 @@ import os
 
 for i in range(12):
     # print('sbatch --partition=west -o out/partdiff-{}c-%A.out -N1 -n1 timescript {}'.format(i+1, i+1))
-    os.system('sbatch --partition=west -o out/partdiff-{}c-%A.out -N1 -n1 timescript {}'.format(i+1))
+    os.system('sbatch --partition=west -o out/partdiff-{}c-%A.out -N1 -n1 timescript {}'.format(i+1, i+1))
